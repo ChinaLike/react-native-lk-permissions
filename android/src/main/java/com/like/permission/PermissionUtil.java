4168d564a62f5c48e2da49dc1ec4c15b85be374f
LiKe <like@LiKedeMBP.lan>
2019-09-03 15:32:41 +0800

    1.更新支持Androidx

--- android/src/main/java/com/like/permission/PermissionUtil.java
+++ android/src/main/java/com/like/permission/PermissionUtil.java
@@ -4,8 +4,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 /**
  * 作者：like on 2019-05-06 11:25
